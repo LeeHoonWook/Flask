249e67f884707dc1af2e4bc390608a0f9ee26ea1
LeeHoonWook <gnsdnr1022@naver.com>
2022-07-16 19:38:59 +0900

    기본 route 알아보기

--- app/__init__.py
+++ app/__init__.py
@@ -8,4 +8,33 @@ def create_app():
     def hello_world():
         return "<p>Hello, World!</p>"
 
+    ''' === Routing Practice === '''
+    from flask import jsonify, redirect, url_for
+    from markupsafe import escape
+
+    @app.route('/test/name/<name>')
+    def name(name):
+        return f'Name is {name}, {escape(type(name))}'
+
+    # 기본값은 string을 반환
+    @app.route('/test/id/<int:id>')
+    def id(id):
+        return 'Id: %d' % id
+
+    @app.route('/test/path/<path:subpath>')
+    def path(subpath):
+        return subpath
+
+    @app.route('/test/json')
+    def json():
+        return jsonify({'hello': 'world'})
+
+    @app.route('/test/redirect/<path:subpath>')
+    def redirect_url(subpath):
+        return redirect(subpath)
+
+    @app.route('/test/urlfor/<path:subpath>')
+    def urlfor(subpath):
+        return redirect(url_for('path', subpath=subpath))
+
     return app
