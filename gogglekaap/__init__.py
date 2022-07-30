969a58492c387480e4c3ba04259b67f7d78b50b3
LeeHoonWook <gnsdnr1022@naver.com>
2022-07-30 14:47:14 +0900

    config 설정 분리

--- gogglekaap/__init__.py
+++ gogglekaap/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, g
 from flask_wtf.csrf import CSRFProtect
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-import os
 
 
 csrf = CSRFProtect()
@@ -10,22 +9,19 @@ db = SQLAlchemy()
 migrate = Migrate()
 
 
-def create_app():
+def create_app(config=None):
     app = Flask(__name__)
 
-    app.config['SECRET_KEY'] = os.environ.get(
-        "SECRET_KEY")
-    app.config['SESSION_COOKIE_NAME'] = 'gogglekaap'
-    # 본인의 DB 설정에 맞추기!
-    # SQLALCHEMY_DATABASE_URI='mysql+pymysql://계정:비밀번호@localhost/DB_Name?charset=utf8'
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
-        "SQLALCHEMY_DATABASE_URI")
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SWAGGER_UI_DOC_EXPANSION'] = 'list'
+    """ === Flask Configuration === """
+    from .configs import DevelopmentConfig, ProductionConfig
+    if not config:
+        if app.config['DEBUG']:
+            config = DevelopmentConfig()
+        else:
+            config = ProductionConfig()
 
-    if app.config['DEBUG'] == True:
-        app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 1
-        app.config['WTF_CSRF_ENABLED'] = False
+    print('run with: ', config)
+    app.config.from_object(config)
 
     """ === DB Init === """
     db.init_app(app)
---
+++ gogglekaap/configs.py
@@ -0,0 +1,26 @@
+import os
+
+
+class Config(object):
+    """Flask Config"""
+
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SESSION_COOKIE_NAME = 'gogglekaap'
+    # 본인의 DB 설정에 맞추기!
+    # SQLALCHEMY_DATABASE_URI='mysql+pymysql://계정:비밀번호@localhost/DB_Name?charset=utf8'
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SWAGGER_UI_DOC_EXPANSION = 'list'
+
+
+class DevelopmentConfig(Config):
+    """Flask Config for Dev"""
+    DEBUG = True
+    SEND_FILE_MAX_AGE_DEFAULT = 1
+    # TODO: Front호출시 토큰 삽입
+    WTF_CSRF_ENABLED = False
+
+
+class ProductionConfig(Config):
+    """Flask Config for Production"""
+    pass
